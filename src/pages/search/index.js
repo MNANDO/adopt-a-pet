19d44996c81ed8eba3c5ca8c1d9c0a3f9bc118b9
Michael F Hernando <138165209+CISSMFH@users.noreply.github.com>
2023-09-04 18:53:47 -0400

    Implemented react router

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -3,40 +3,42 @@ import Hero from '../../components/hero';
 import { getPets } from '../../api/petfinder';
 import Pet from '../../components/pet';
 // Import useSearchParams
+import { useSearchParams } from 'react-router-dom';
 
 const SearchPage = () => {
 
-  // Get searchParams object from useSearchParams
+    // Get searchParams object from useSearchParams
+    const [ searchParams ] = useSearchParams();
 
-  const petNameToFind = 'REPLACE ME';  // Get query parameter using searchParams object
+    const petNameToFind = searchParams.get('name');  // Get query parameter using searchParams object
 
-  const [pets, setPets] = useState([]);
+    const [pets, setPets] = useState([]);
 
-  useEffect(() => {
-    async function getPetsData() {
-      const petsData = await getPets('', petNameToFind);
+    useEffect(() => {
+        async function getPetsData() {
+            const petsData = await getPets('', petNameToFind);
 
-      setPets(petsData);
-    }
+            setPets(petsData);
+        }
 
-    getPetsData();
-  }, [petNameToFind]);
+        getPetsData();
+    }, [petNameToFind]);
 
-  return (
-    <div className="page">
-      <Hero displayText={`Results for ${petNameToFind}`} />
+    return (
+        <div className="page">
+        <Hero displayText={`Results for ${petNameToFind}`} />
 
-      <h3>Pets available for adoption near you</h3>
+        <h3>Pets available for adoption near you</h3>
 
-      <main>
+        <main>
         <div className="grid">
-          {pets.map((pet) => (
+        {pets.map((pet) => (
             <Pet animal={pet} key={pet.id} />
-          ))}
+        ))}
         </div>
-      </main>
-    </div>
-  );
+        </main>
+        </div>
+    );
 };
 
 export default SearchPage;
