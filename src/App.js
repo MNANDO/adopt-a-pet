621d3f9bf605650883743fd338b4e197fb911991
Michael F Hernando <138165209+CISSMFH@users.noreply.github.com>
2023-10-30 13:14:18 -0400

    s

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } 
 
 // create router with JSX Route elements
 const appRouter = createBrowserRouter(createRoutesFromElements(
-    <Route path="/" element={ <Root /> }>
+    <Route path="/adopt-a-pet/" element={ <Root /> }>
         <Route index element={ <HomePage /> } />
         <Route path=":type" element={ <HomePage /> } />
         <Route path=":type/:id" element={ <PetDetailsPage />} />
